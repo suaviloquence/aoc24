9c2fad6ae340e50ff6d8c722f3bd0dab956bd165
m <m@mcarr.one>
2024-12-03 21:27:49 -0800

    put impl outside of body for ide support

--- src/day9.rs
+++ src/day9.rs
@@ -1,18 +1,32 @@
 use aoc_runner_derive::{aoc, aoc_generator};
 
-type Inp = ();
+type Inp = String;
+
+fn parse_inner(input: &str) -> Inp {
+    input.to_string()
+}
+
+type P1 = usize;
+fn p1_inner(input: &Inp) -> P1 {
+    todo!()
+}
+
+type P2 = usize;
+fn p2_inner(input: &Inp) -> P2 {
+    todo!()
+}
 
 #[aoc_generator(day9)]
 pub fn parse(input: &str) -> Inp {
-    todo!()
+    parse_inner(input)
 }
 
 #[aoc(day9, part1)]
 pub fn p1(input: &Inp) -> usize {
-    todo!()
+    p1_inner(input)
 }
 
 #[aoc(day9, part2)]
 pub fn p2(input: &Inp) -> usize {
-    todo!() as usize
+    p2_inner(input)
 }
