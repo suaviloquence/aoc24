50fcae017e7368d3e60f1b3ba9814999bb32d05b
m <m@mcarr.one>
2024-12-04 21:18:44 -0800

    day 5 fixpoint

--- src/day5.rs
+++ src/day5.rs
@@ -1,19 +1,79 @@
 use aoc_runner_derive::{aoc, aoc_generator};
 
-type Inp = String;
+type Inp = (Vec<(usize, usize)>, Vec<Vec<usize>>);
 
 fn parse_inner(input: &str) -> Inp {
-    input.to_string()
+    let (a, b) = input.split_once("\n\n").unwrap();
+
+    let a = a
+        .lines()
+        .map(|x| {
+            let (c, d) = x.split_once("|").unwrap();
+            (c.parse().unwrap(), d.parse().unwrap())
+        })
+        .collect();
+
+    let b = b
+        .lines()
+        .map(|x| x.split(",").map(|x| x.parse().unwrap()).collect())
+        .collect();
+
+    (a, b)
 }
 
 type P1 = usize;
-fn p1_inner(input: &Inp) -> P1 {
-    todo!()
+fn p1_inner((rules, orders): &Inp) -> P1 {
+    orders
+        .iter()
+        .filter(|r| {
+            r.iter().enumerate().all(|(i, x)| {
+                rules
+                    .iter()
+                    .filter(|y| y.0 == *x)
+                    .map(|x| x.1)
+                    .all(|z| !r[..i].contains(&z))
+            })
+        })
+        .map(|x| x[x.len() / 2])
+        .sum()
 }
 
 type P2 = usize;
-fn p2_inner(input: &Inp) -> P2 {
-    todo!()
+fn p2_inner((rules, orders): &Inp) -> P2 {
+    let inc = orders.iter().filter(|r| {
+        !r.iter().enumerate().all(|(i, x)| {
+            rules
+                .iter()
+                .filter(|y| y.0 == *x)
+                .map(|x| x.1)
+                .all(|z| !r[..i].contains(&z))
+        })
+    });
+
+    inc.map(|x| {
+        let mut vec = x.iter().copied().collect::<Vec<_>>();
+        let mut changed = true;
+
+        while changed {
+            changed = false;
+            for i in 0..vec.len() {
+                if let Some((j, _)) = rules
+                    .iter()
+                    .filter(|y| y.0 == vec[i])
+                    .find_map(|(_, z)| vec[..i].iter().enumerate().find(|(_, j)| *j == z))
+                {
+                    let x = vec[i];
+                    vec[i] = vec[j];
+                    vec[j] = x;
+                    changed = true;
+                }
+            }
+        }
+
+        vec
+    })
+    .map(|x| x[x.len() / 2])
+    .sum()
 }
 
 #[aoc_generator(day5)]
--- src/lib.rs
+++ src/lib.rs
@@ -20,7 +20,7 @@ pub mod day2;
 // pub mod day25;
 pub mod day3;
 pub mod day4;
-// pub mod day5;
+pub mod day5;
 // pub mod day6;
 // pub mod day7;
 // pub mod day8;
