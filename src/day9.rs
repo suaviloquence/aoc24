3ba885c90cdfc5c42f2001d9aa86544a00629cd7
m <m@mcarr.one>
2024-12-08 21:37:32 -0800

    day 9

--- src/day9.rs
+++ src/day9.rs
@@ -1,19 +1,105 @@
 use aoc_runner_derive::{aoc, aoc_generator};
 
-type Inp = String;
+type Inp = Vec<(bool, u8, usize)>;
 
 fn parse_inner(input: &str) -> Inp {
-    input.to_string()
+    input
+        .chars()
+        .enumerate()
+        .map(|(i, x)| (i % 2 == 0, x.to_digit(10).unwrap() as u8, i / 2))
+        .collect()
 }
 
 type P1 = usize;
 fn p1_inner(input: &Inp) -> P1 {
-    todo!()
+    let mut input = input
+        .iter()
+        .copied()
+        .flat_map(|(i, j, k)| (0..j).map(move |_| (i, k)))
+        .collect::<Vec<_>>();
+
+    let mut i = 0;
+    let mut j = input.len() - 1;
+
+    while j > i {
+        while input[i].0 {
+            i += 1;
+            if i == input.len() {
+                break;
+            }
+        }
+
+        while !input[j].0 {
+            if j == 0 {
+                break;
+            }
+
+            j -= 1;
+        }
+
+        let x = input[i];
+        input[i] = input[j];
+        input[j] = x;
+
+        if j == 0 {
+            break;
+        }
+
+        j -= 1;
+        i += 1;
+    }
+
+    input
+        .into_iter()
+        .filter(|x| x.0)
+        .enumerate()
+        .map(|(i, (_, j))| i * j)
+        .sum()
 }
 
 type P2 = usize;
 fn p2_inner(input: &Inp) -> P2 {
-    todo!()
+    let mut input = input
+        .iter()
+        .copied()
+        .flat_map(|(i, j, k)| (0..j).map(move |_| i.then_some(k)))
+        .collect::<Vec<_>>();
+
+    let mut j = input.len() - 1;
+
+    while (0..input.len()).contains(&j) {
+        while input[j].is_none() {
+            if j == 0 {
+                break;
+            }
+
+            j -= 1;
+        }
+        let Some(len) = (0..j).find(|&x| input[j - x] != input[j]) else {
+            break;
+        };
+
+        let Some(i) = (0..j + 1 - len).find(|&i| input[i..i + len].iter().all(Option::is_none))
+        else {
+            j -= len;
+            continue;
+        };
+
+        for n in 0..len {
+            let x = input[i + n];
+            input[i + n] = input[j - n];
+            input[j - n] = x;
+        }
+
+        j -= len;
+    }
+
+    input
+        .into_iter()
+        .enumerate()
+        .filter_map(|(i, x)| x.map(move |j| (i, j)))
+        .map(|(i, j)| i * j)
+        .sum()
 }
 
 #[aoc_generator(day9)]
@@ -29,4 +115,9 @@ pub fn p1(input: &Inp) -> usize {
 #[aoc(day9, part2)]
 pub fn p2(input: &Inp) -> usize {
     p2_inner(input)
+}
+
+#[test]
+fn test() {
+    panic!("{}", p2(&parse("2333133121414131402")))
 }
--- src/lib.rs
+++ src/lib.rs
@@ -24,7 +24,7 @@ pub mod day5;
 pub mod day6;
 pub mod day7;
 // pub mod day8;
-// pub mod day9;
+pub mod day9;
 
 pub mod utils;
 
