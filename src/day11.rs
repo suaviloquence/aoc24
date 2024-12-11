12eca0f3beb0de80a6c18c25fd591b3a77a8653c
m <m@mcarr.one>
2024-12-10 21:24:38 -0800

    day 11

--- src/day11.rs
+++ src/day11.rs
@@ -1,19 +1,69 @@
+use std::collections::BTreeMap;
+
 use aoc_runner_derive::{aoc, aoc_generator};
 
-type Inp = String;
+type Inp = Vec<usize>;
 
-fn parse_inner(input: &str) -> Inp {
-    input.to_string()
+fn parse_inner(input: &str) -> Vec<usize> {
+    input
+        .split_ascii_whitespace()
+        .map(|x| x.parse().unwrap())
+        .collect()
 }
 
 type P1 = usize;
 fn p1_inner(input: &Inp) -> P1 {
-    todo!()
+    let mut prev = input.clone();
+    let mut next;
+
+    for _ in 0..25 {
+        next = Vec::with_capacity(prev.len());
+        for st in prev {
+            if st == 0 {
+                next.push(1);
+            } else {
+                let fmt = st.to_string();
+                if fmt.len() % 2 == 0 {
+                    next.push(fmt[..fmt.len() / 2].parse().unwrap());
+                    next.push(fmt[fmt.len() / 2..].parse().unwrap());
+                } else {
+                    next.push(st * 2024);
+                }
+            }
+        }
+
+        prev = next;
+    }
+
+    prev.len()
 }
 
 type P2 = usize;
 fn p2_inner(input: &Inp) -> P2 {
-    todo!()
+    let mut prev = input.iter().map(|&x| (x, 1)).collect::<BTreeMap<_, _>>();
+
+    for i in 0..75 {
+        println!("{i}");
+        let mut next = BTreeMap::new();
+
+        for (st, i) in prev {
+            if st == 0 {
+                *next.entry(1).or_default() += i;
+            } else if ((st as f64).log10() + f64::EPSILON).ceil() as usize % 2 == 0 {
+                let str = st.to_string();
+                let a = str[..str.len() / 2].parse().unwrap();
+                let b = str[str.len() / 2..].parse().unwrap();
+                *next.entry(a).or_default() += i;
+                *next.entry(b).or_default() += i;
+            } else {
+                *next.entry(st * 2024).or_default() += i;
+            }
+        }
+
+        prev = next;
+    }
+
+    prev.into_values().sum()
 }
 
 #[aoc_generator(day11)]
